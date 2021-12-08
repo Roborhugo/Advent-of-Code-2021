dabf50b6af94728dc00901eabadaf1738878a0a2
Roborhugo <Robothugoand@gmail.com>
2021-12-08 23:41:35 +0100

    Fixed input and created main function

--- Day 01/increases.scala
+++ Day 01/increases.scala
@@ -1,8 +1,15 @@
 package day01
 
-/** puzzleInput as a workable Vector[Int]*/
-val input: Vector[Int] = 
-  puzzleInput.split(' ').map(_.toInt).toVector
+@main
+def start(file: String): Unit = 
+  println(countIncreases(input(file)))
+
+/** Input as a workable Vector[Int]*/
+def input(filename: String): Vector[Int] = 
+  import scala.io.Source // To read from unput file
+
+  val lines = for line <- Source.fromFile(filename).getLines yield line
+  lines.toVector.map(_.toInt)
 
 /** Counts number of increases in a depth measurement */
 def countIncreases(depthMeasurements: Vector[Int]): Int = 
