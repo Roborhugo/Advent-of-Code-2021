e143286275e3022b9c35644b819ce1d434352ad0
Roborhugo <Robothugoand@gmail.com>
2021-12-09 23:50:26 +0100

    Added more output and compensated for new Submarine clas

--- Day 02/main.scala
+++ Day 02/main.scala
@@ -3,16 +3,21 @@ package day02
 /** Starts program*/
 @main
 def main(filename: String) = 
-  movement(input(filename))
+  val submarine = new Submarine
+  val aimedSubmarine = new AimedSubmarine
 
-  println(s"Final depth * position: ${Submarine.depth * Submarine.position}")
+  movement(input(filename), submarine)
+  movement(input(filename), aimedSubmarine)
+
+  println(s"Final depth * position of Submarine: ${submarine.depth * submarine.position}")
+  println(s"Final depth * position of Aimed Submarine: ${aimedSubmarine.depth * aimedSubmarine.position}")
 
 /** Moves submarine using planned course (input) */
-def movement(course: Vector[(String, Int)]): Unit = 
+def movement(course: Vector[(String, Int)], submarine: Submarine): Unit = 
   course.foreach(command => command._1 match
-    case "forward" => Submarine.forward(command._2)
-    case "down" => Submarine.down(command._2)
-    case "up" => Submarine.up(command._2))
+    case "forward" => submarine.forward(command._2)
+    case "down" => submarine.down(command._2)
+    case "up" => submarine.up(command._2))
 
 /** Gets input as workable Vector*/
 def input(filename: String): Vector[(String, Int)] = 
