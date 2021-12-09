78891f937746a5f64c841769e0d2bb31d350e38f
Roborhugo <Robothugoand@gmail.com>
2021-12-09 22:31:15 +0100

    Created submarine singleton

--- Day 02/submarine.scala
+++ Day 02/submarine.scala
@@ -1,1 +1,14 @@
 package day02
+
+object Submarine:
+  var depth = 0
+  var position = 0
+
+  def forward(distance: Int) = 
+    position += distance
+
+  def up(distance: Int) = 
+    depth -= distance
+
+  def down(distance: Int) = 
+    depth += distance
