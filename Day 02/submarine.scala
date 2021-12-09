0703e15f8649079a11fc8db4001eef2b2208a4aa
Roborhugo <Robothugoand@gmail.com>
2021-12-09 23:21:28 +0100

    Added AimedSubmarine

--- Day 02/submarine.scala
+++ Day 02/submarine.scala
@@ -17,3 +17,23 @@ object Submarine:
   /** Moves submarine distance steps down*/
   def down(distance: Int): Unit = 
     depth += distance
+
+
+object AimedSubmarine:
+  var depth = 0
+  var position = 0
+  var aim = 0
+
+  /** Moves submarine distance steps forward and distance * */
+  def forward(distance: Int): Unit = 
+    position += distance
+    depth += distance * aim
+
+  /** Rotates submarine up by rotation*/
+  def up(rotation: Int): Unit = 
+    aim -= rotation
+
+
+  /** Moves submarine distance steps down*/
+  def down(rotation: Int): Unit = 
+    aim += rotation
