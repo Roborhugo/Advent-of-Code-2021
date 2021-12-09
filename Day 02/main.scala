6c50301a03cb143e40829b8e8c7edd8d4a0f9d62
Roborhugo <Robothugoand@gmail.com>
2021-12-09 22:56:47 +0100

    Added input

--- Day 02/main.scala
+++ Day 02/main.scala
@@ -6,3 +6,11 @@ def movement(course: Vector[(String, Int)]): Unit =
     case "forward" => Submarine.forward(command._2)
     case "down" => Submarine.down(command._2)
     case "up" => Submarine.up(command._2))
+
+/** Gets input as workable Vector*/
+def input(filename: String): Vector[(String, Int)] = 
+  import scala.io.Source // To read from unput file
+
+  val lines: Vector[String] = for line <- Source.fromFile(filename).getLines yield line
+  
+  lines.map(c => (c.split(' ')(0), c.split(' ')(1)))
