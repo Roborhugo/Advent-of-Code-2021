867256d931a3014cce2f90eb3489157d39a7f608
Roborhugo <Robothugoand@gmail.com>
2021-12-09 22:33:37 +0100

    Added comments to code

--- Day 02/submarine.scala
+++ Day 02/submarine.scala
@@ -1,14 +1,19 @@
 package day02
 
+/** Model of our submarine*/
 object Submarine:
   var depth = 0
   var position = 0
 
+  /** Moves submarine distance steps forward*/
   def forward(distance: Int) = 
     position += distance
-
+  
+  /** Moves submarine distance steps up*/
   def up(distance: Int) = 
     depth -= distance
 
+
+  /** Moves submarine distance steps down*/
   def down(distance: Int) = 
     depth += distance
