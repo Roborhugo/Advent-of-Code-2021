8b9cace20b5b4c0be9285f888253d065fd849e9a
Roborhugo <Robothugoand@gmail.com>
2021-12-09 22:42:33 +0100

    Created movement

--- Day 02/main.scala
+++ Day 02/main.scala
@@ -1,1 +1,8 @@
 package day02
+
+/** Moves submarine using planned course (input) */
+def movement(course: Vector[(String, Int)]): Unit = 
+  course.foreach(command => command._1 match
+    case "forward" => Submarine.forward(command._2)
+    case "down" => Submarine.down(command._2)
+    case "up" => Submarine.up(command._2))
